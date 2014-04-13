70f131dee1e8663f3c50d72cfead5325a5eef26d
Samuel Reed <sam@tixelated.com>
2014-04-13 17:54:38 +0800

    Art

---
+++ USAGE.md
@@ -0,0 +1,37 @@
+Usage
+=====
+
+The proxy server has been started on port <port>.
+
+You need to point your browser to this proxy server to travel 
+back in time to <date>.
+
+Proxy Server configuration
+--------------------------
+
+### Firefox
+
+To configure the proxy server, navigate to:
+
+Preferences -> Advanced -> Network -> Connection Settings
+
+**Manual Proxy Configuration**: 
+
+* HTTP Proxy: 127.0.0.1
+* Port: <port>
+
+### Other Browsers
+
+Chrome, Safari, and IE use the system proxy settings. In all 
+operating systems you can configure an HTTP proxy at 
+127.0.0.1:<port> to access the wayback-machine-machine.
+
+
+Notes
+-----
+
+The proxy server is now running. Press CTRL-C to exit.
+
+If you liked this project and have feedback or bugs to report,
+please contact me at sam@tixelated.com or on the GitHub issues
+page at github.com/STRML/wayback-machine-machine.
--- app.js
+++ app.js
@@ -6,19 +6,26 @@ var args = require('yargs')
   .demand(1)
   .argv;
 
-var date = args._[0].replace(/-/g, '');
+// Get options
+var date = args._[0];
 var port = args.port;
 var log = require('./lib/log').init(args.debug).log;
 
-console.log("Running on port %s with date %s", port, date);
+// Print banner & Usage
+var ascli = require('ascli').app(require('./package').name.replace(/-/g, '  '));
+ascli.banner(ascli.appName.rainbow.bold);
+var msee = require('msee');
+var usage = msee.parseFile(process.cwd() + '/USAGE.md');
+console.log(usage.replace(/<port>/g, port).replace(/<date>/g, new Date(date).toLocaleDateString()));
 
+// Start local proxy server.
 var proxyToWayback = require('./lib/proxy');
 var http = require('http');
 var express = require('express');
 var app = express();
 
 app.use(require('cookie-parser')());
-app.use(proxyToWayback.bind(null, {date: date}));
+app.use(proxyToWayback.bind(null, {date: date.replace(/-/g, '')}));
 
 app.on('error', function(err) {
   console.error("Unable to open port:", port);
